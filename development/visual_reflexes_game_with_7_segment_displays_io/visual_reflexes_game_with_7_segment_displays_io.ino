07ac99feab7217727421d0aaca3f827683d87212
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:42:04 +0300

    Change.

--- development/visual_reflexes_game_with_7_segment_displays_io/visual_reflexes_game_with_7_segment_displays_io.ino
+++ development/visual_reflexes_game_with_7_segment_displays_io/visual_reflexes_game_with_7_segment_displays_io.ino
@@ -1,7 +1,7 @@
 /*
  *  Visual Reflexes Game With 7-Segment Displays & I/O.
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
